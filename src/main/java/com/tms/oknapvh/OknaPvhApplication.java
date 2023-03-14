91ee24878aaa851b68ec52bf16d801d2d7ccc9af
Danila Rebkovets <danila.rebkovets20@gmail.com>
2023-03-14 22:41:04 +0300

    version 1.0

---
+++ src/main/java/com/tms/oknapvh/OknaPvhApplication.java
@@ -0,0 +1,13 @@
+package com.tms.oknapvh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OknaPvhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OknaPvhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tms/oknapvh/OknaPvhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tms.oknapvh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OknaPvhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
