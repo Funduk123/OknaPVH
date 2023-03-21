dbb67b24be44db46dea02775b98d4f4e46373ee5
Danila Rebkovets <danila.rebkovets20@gmail.com>
2023-03-21 22:46:56 +0300

    version 1.4.5

--- src/main/java/com/tms/oknapvh/mapper/UserMapper.java
+++ src/main/java/com/tms/oknapvh/mapper/UserMapper.java
@@ -3,8 +3,11 @@ package com.tms.oknapvh.mapper;
 import com.tms.oknapvh.dto.UserDto;
 import com.tms.oknapvh.entity.UserEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.MapperConfig;
+import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring")
+@MapperConfig(unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
 
     UserEntity dtoToEntity(UserDto userDto);
--- src/main/java/com/tms/oknapvh/mapper/WindowMapper.java
+++ src/main/java/com/tms/oknapvh/mapper/WindowMapper.java
@@ -3,8 +3,11 @@ package com.tms.oknapvh.mapper;
 import com.tms.oknapvh.dto.WindowDto;
 import com.tms.oknapvh.entity.WindowEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.MapperConfig;
+import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring")
+@MapperConfig(unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface WindowMapper {
 
     WindowEntity dtoToEntity(WindowDto windowDto);
